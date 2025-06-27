f1324e9c43baadad03861e338e2d18c8964da4b6
Bemental77 <bementcw@gmail.com>
2025-06-26 21:45:31 -0500

    added compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -36,9 +37,6 @@ android {
     buildFeatures {
         prefab = true
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
     }
     externalNativeBuild {
         cmake {
